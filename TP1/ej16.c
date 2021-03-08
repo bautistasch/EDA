3017139955ff34f0f66a179bb71f8027af1e5670
bautistasch <bschneeberger@itba.edu.ar>
2021-03-08 14:55:43 -0300

    agregue una forma interesante de testeo, con matriz de ptrs

--- TP1/ej16.c
+++ TP1/ej16.c
@@ -26,15 +26,18 @@ typedef struct
         char parametro[STR_SIZE];
         char clave[STR_SIZE];
     };
-        int valor;
-}userDataInput;
+    int valor;
+}userDataInput_t;
 
 
 int main()
 {
-    userDataInput myData[MAX_DATA];
+    userDataInput_t myData[MAX_DATA];
 
-    char * argv[] = {"./myProgram","-div", "3", "param1", "param2", "param3", "-sum", "2", "-div", "-4", "-res", "2"};
+/*
+    //########### Test 1 #############
+
+    char * argv[] = {"./myProgram","-div", "3", "param1", "param2", "param3", "-sum", "2", "-div", "-4", "-res", "2", "param3"};
     int argc = sizeof(argv)/sizeof(char *);
 
     for (int i = 0; i < MAX_DATA; i++)
@@ -43,28 +46,15 @@ int main()
         myData[i].errorState = NO_ERROR;
     }
     int state = parseCmdLine(argc, argv, &parseCallback, myData );
-    if( state != OK)
+    if( state != -1)
     {
-        switch (state)
-        {
-        case ERROR_1:
-            printf("ERROR 1\n");
-            break;
-        case ERROR_2:
-            printf("ERROR 2\n");
-            break;
-        case ERROR_CALLBACK:
-            printf("ERROR CALLBACK\n");
-            break;
-        }
-    }
-    else
-    {
+        printf("La cantidad de opciones y parametros ingresada fue de = %d\n", state);
+
         for (int i = 0; i < MAX_DATA && myData[i].parametro != VOID; i++)
         {
             if(myData[i].errorState == DATA_OVERFLOW || myData[i].errorState == WRONG_ARG )
             {
-                printf("ERROR DE INPUT %d\n", i);
+                printf("ERROR DE INPUT %d\n", i);                  // Esto habria que sacarlo porque si entra aca es porque no hay errores
             }
             else if(myData[i].tipoDeArgumento == OPCION)
             {
@@ -78,12 +68,118 @@ int main()
         }
     }
 
+    //########### Test 2 #############
+
+    char * argv2[] = {"./myProgram","-div", "3", "param1", "param2", "-"};
+    argc = sizeof(argv2)*sizeof(char *);
+
+    for (int i = 0; i < MAX_DATA; i++)
+    {
+        myData[i].tipoDeArgumento = VOID;
+        myData[i].errorState = NO_ERROR;
+    }    
+
+    printf("\nTEST 2 BEGIN \n");
+
+    state = parseCmdLine(argc, argv2, &parseCallback, myData );
+    if( state != -1)
+    {
+        printf("La cantidad de opciones y parametros ingresada fue de = %d\n", state);
+
+        for (int i = 0; i < MAX_DATA && myData[i].parametro != VOID; i++)
+        {
+            if(myData[i].errorState == DATA_OVERFLOW || myData[i].errorState == WRONG_ARG )
+            {
+                printf("ERROR DE INPUT %d\n", i);                  // Esto habria que sacarlo porque si entra aca es porque no hay errores
+            }
+            else if(myData[i].tipoDeArgumento == OPCION)
+            {
+                printf("CLAVE = %s\n", myData[i].clave);
+                printf("VALOR = %d\n\n\n", myData[i].valor);
+            }
+            else if(myData[i].tipoDeArgumento == PARAMETRO)
+            {
+                printf("PARAMETRO = %s\n\n\n", myData[i].parametro);
+            }
+        }
+    }
+*/
+    //########### Test BORRABLE #############
+
+    printf("\nTEST BORRABLE BEGIN \n\n");
+
+    char * argv3[30][30] = {{"./myProgram","-div", "3", "param1", "param2", "-", NULL},
+                          {"./myProgram","-div", "3", "param1", "param2", "param3", "-sum", "2", "-div", "-4", "-res", "2", "param3", NULL  },
+                          {"./myProgram","-div", "3", NULL},
+                          {"./myProgram","-div", "3", NULL},
+                          {"./myProgram", "-", NULL} ,
+                          {NULL}  };
+
+    int cols = 0;
+    while(argv3[cols][0] != NULL)
+        cols++;
+
+    if(cols == 30)
+    {
+        printf("ERROR DEL TESTER\n");
+    }
+    else
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("\n\n#####TEST NRO %d ha comenenzado#####\n\n", j);
+    
+            int argc3 = 0;
+    
+            while(argv3[j][argc3] != NULL && argc3 < 30)
+                argc3++;
+    
+            if(argc3 < 30 && argv3[j][argc3] == NULL)
+            {
+            
+                for (int i = 0; i < MAX_DATA; i++)
+                {
+                    myData[i].tipoDeArgumento = VOID;
+                    myData[i].errorState = NO_ERROR;
+                }    
+                int state = parseCmdLine(argc3, argv3[j], &parseCallback, myData );
+                if( state != -1)
+                {
+                    printf("La cantidad de opciones y parametros ingresada fue de = %d\n", state);
+    
+                    for (int i = 0; i < MAX_DATA && myData[i].parametro != VOID; i++)
+                    {
+                        if(myData[i].errorState == DATA_OVERFLOW || myData[i].errorState == WRONG_ARG )
+                        {
+                            printf("ERROR DE INPUT %d\n", i);                  // Esto habria que sacarlo porque si entra aca es porque no hay errores
+                        }
+                        else if(myData[i].tipoDeArgumento == OPCION)
+                        {
+                            printf("CLAVE = %s\n", myData[i].clave);
+                            printf("VALOR = %d\n\n", myData[i].valor);
+                        }
+                        else if(myData[i].tipoDeArgumento == PARAMETRO)
+                        {
+                            printf("PARAMETRO = %s\n\n", myData[i].parametro);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                printf("ERROR SISTEMA DE TESTEO");
+            }
+        }
+    }    
+
+
 }
 
 int parseCmdLine(int argc, char * argv[], pCallback p, void * userData)
 {
     int errorState = OK;
     int state = STATE_0;
+    int dataCounter = 0;
     for (int i = 1; i < argc && errorState == OK ; i++)
     {
         switch (state)
@@ -94,6 +190,7 @@ int parseCmdLine(int argc, char * argv[], pCallback p, void * userData)
                if(argv[i][1] == '\0')
                {
                    errorState = ERROR_1; // NOT SURE
+                   printf("ERROR 1\n");
                }
                else
                {
@@ -103,27 +200,42 @@ int parseCmdLine(int argc, char * argv[], pCallback p, void * userData)
             else
             {
                 if(p( NULL, argv[i], userData) == 0)
+                {
                     errorState = ERROR_CALLBACK;
+                    printf("ERROR CALLBACK\n");
+                }
+                else
+                {
+                    dataCounter++;
+                }
             }
             break;
 
         case STATE_1:     // tomo el valor de la opcion
             state = STATE_0;
             if(p(argv[i - 1], argv[i], userData) == 0 )
+            {
                 errorState = ERROR_CALLBACK;
+                printf("ERROR CALBACK\n");
+            }
+            else
+            {
+                dataCounter++;
+            }
             break;
         }
     }
     if(state == STATE_1)
     {
         errorState = ERROR_2;
+        printf("ERROR 2\n");
     }
-    return errorState;
+    return (errorState == 0 ? dataCounter : -1);
 }
 
 int parseCallback(char * key, char * value, void * userData)
 {
-    userDataInput * pUserData = userData;
+    userDataInput_t * pUserData = userData;
     int error = 0;
 
     int i = 0;
