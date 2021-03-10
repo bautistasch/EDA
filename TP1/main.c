9721f84f57130e3a1043a38ec4fb2713330205e7
bautistasch <62912239+bautistasch@users.noreply.github.com>
2021-03-09 23:00:52 -0300

    Add files via upload

--- TP1/main.c
+++ TP1/main.c
@@ -1,84 +1,49 @@
-/***************************************************************************//**
-  @file     main.c
-  @author   Grupo7
- ******************************************************************************/
-
- /*******************************************************************************
- * INCLUDE HEADER FILES
- ******************************************************************************/
- 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "parseArg.h"
-
-/*******************************************************************************
- * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
- ******************************************************************************/
-
-#define MAX_DATA  40    // Max size of the auxiliary matrix 
-#define STR_SIZE  40    
 
 #define MAX_MATRIX 30
 
-/*******************************************************************************
- * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
- ******************************************************************************/
-
-enum{OK = 0, ERROR_1, ERROR_2, ERROR_CALLBACK}; // enums that belongs to the callback
-enum{STATE_0, STATE_1};
-
-
-typedef enum{NO_ERROR, DATA_OVERFLOW, WRONG_ARG} errorStates_t;    // calback variables 
+typedef enum{NO_ERROR, WRONG_ARG} errorStates_t;    // calback variables 
 typedef enum{VOID, PARAMETRO, OPCION} tipoDeArgumento_t; 
+
 
 typedef struct 
 {
     tipoDeArgumento_t tipoDeArgumento;
     errorStates_t errorState;
     union{
-        char parametro[STR_SIZE];
-        char clave[STR_SIZE];
+        char * parametro;
+        char * clave;
     };
     int valor;
 }userDataInput_t;
 
-/*******************************************************************************
- * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
- ******************************************************************************/
-
-static int parseCallback(char * key, char * value, void * userData);
-
-/*******************************************************************************
- *******************************************************************************
-                        LOCAL FUNCTION DEFINITIONS
- *******************************************************************************
- ******************************************************************************/
+int parseCallback(char * key, char * value, void * userData);
 
 int main()
 {
-    userDataInput_t myData[MAX_DATA];
-
 
     printf("\nTEST BORRABLE BEGIN \n\n");
 
-    char * argv3[MAX_MATRIX][MAX_MATRIX] = {{"./myProgram","-div", "3", "param1", "param2", "-", NULL},
-                          {"./myProgram","-div", "3", "param1", "param2", "param3", "-sum", "2", "-div", "-4", "-res", "2", "param3", NULL  },
-                          {"./myProgram","-div", "3", NULL},
-                          {"./myProgram","-div", "3", NULL},
-                          {"./myProgram", "-hola", NULL} ,
+    char * argv3[MAX_MATRIX][MAX_MATRIX] = {{"./myProgram","-clave1", "3", NULL},
+                          {"./myProgram","-clave2", "3", "param1", "param2", "param3", "-clave1", "2", "-clave3", "-4", "-clave2", "2", "param3", NULL  },
+                          {"./myProgram","-clave2", "3", NULL},
+                          {"./myProgram","-clave1", "3", "-", NULL},
+                          {"./myProgram", "-clave2", NULL} ,
                           {"./myProgram", "-", "7", NULL},
                           {"./myProgram", NULL},
                           {"./myProgram", "param1", "mal_escrito", NULL},
                           {NULL}  };
 
     int fils = 0;
-    while(argv3[fils][0] != NULL)
+    while(argv3[fils][0] != NULL && fils < (MAX_MATRIX - 1))    // -1 porque la ultima fila se reserva para la NULL
         fils++;
 
-    if(fils == 30)
+    if(fils == (MAX_MATRIX - 1) )
     {
-        printf("ERROR DEL TESTER\n");
+        printf("ERROR DEL TESTER, demasiados tests ingresados\n");
     }
     else
     {
@@ -88,121 +53,112 @@ int main()
     
             int argc3 = 0;
     
-            while(argv3[j][argc3] != NULL && argc3 < 30)
+            while(argv3[j][argc3] != NULL && argc3 < MAX_MATRIX)
                 argc3++;
     
-            if(argc3 < 30 && argv3[j][argc3] == NULL)
+            userDataInput_t * myPtr = malloc( argc3 * sizeof(userDataInput_t) );
+
+
+            if(argc3 < MAX_MATRIX && argv3[j][argc3] == NULL)
             {
-            
-                for (int i = 0; i < MAX_DATA; i++)
+                for (int i = 0; i < argc3 ; i++)
                 {
-                    myData[i].tipoDeArgumento = VOID;
-                    myData[i].errorState = NO_ERROR;
+                    myPtr[i].tipoDeArgumento = VOID;
+                    myPtr[i].errorState = NO_ERROR;
                 }    
-                int state = parseCmdLine(argc3, argv3[j], &parseCallback, myData );
+
+                int state = parseCmdLine(argc3, argv3[j], &parseCallback, myPtr );
                 if( state != -1)
                 {
                     printf("La cantidad de opciones y parametros ingresada fue de = %d\n", state);
     
-                    for (int i = 0; i < MAX_DATA && myData[i].parametro != VOID; i++)
+                    for (int i = 0; i < argc3 && myPtr[i].parametro != VOID; i++)
                     {
-                        if(myData[i].errorState == DATA_OVERFLOW || myData[i].errorState == WRONG_ARG )
+                        if(myPtr[i].tipoDeArgumento == OPCION)
                         {
-                            printf("ERROR DE INPUT %d\n", i);                  // Esto habria que sacarlo porque si entra aca es porque no hay errores
+                            printf("CLAVE = %s\n", myPtr[i].clave);
+                            printf("VALOR = %d\n\n", myPtr[i].valor);
                         }
-                        else if(myData[i].tipoDeArgumento == OPCION)
+                        else if(myPtr[i].tipoDeArgumento == PARAMETRO)
                         {
-                            printf("CLAVE = %s\n", myData[i].clave);
-                            printf("VALOR = %d\n\n", myData[i].valor);
-                        }
-                        else if(myData[i].tipoDeArgumento == PARAMETRO)
-                        {
-                            printf("PARAMETRO = %s\n\n", myData[i].parametro);
+                            printf("PARAMETRO = %s\n\n", myPtr[i].parametro);
                         }
                     }
                 }
                 else
                 {
-                //Chequeo si el error es de la callback
-                if(myData[0].errorState == WRONG_ARG )
-                    printf("WRONG ARGUMENT\n");
-                else if(myData[0].errorState == DATA_OVERFLOW)
-                    printf("DEMASIADOS ARGUMENTOS INGRESADOS\n");
+                    if(myPtr[0].errorState == WRONG_ARG )
+                        printf("WRONG ARGUMENT\n");
                 }
             }
             else
             {
                 printf("ERROR SISTEMA DE TESTEO\n");
             }
+
+            free(myPtr);
         }
     }    
 }
 
 
-static int parseCallback(char * key, char * value, void * userData)
+int parseCallback(char * key, char * value, void * userData)
 {
     userDataInput_t * pUserData = userData;
     int error = 0;
 
     int i = 0;
-    while(pUserData[i].tipoDeArgumento != VOID && i < MAX_DATA)
+    while(pUserData[i].tipoDeArgumento != VOID )
         i++;
 
-    if(i == MAX_DATA)
+    if(key == NULL)
     {
-        pUserData[0].errorState = DATA_OVERFLOW;
-        error = 1;
-    }
-    else
-    {
-        if(key == NULL)
+        pUserData[i].tipoDeArgumento = PARAMETRO;
+        if(strcmp(value, "param1") == 0)
         {
-            pUserData[i].tipoDeArgumento = PARAMETRO;
-            if(strcmp(value, "param1") == 0)
-            {
-                strcpy(pUserData[i].parametro, "param1");
-            }
-            else if(strcmp(value, "param2") == 0)
-            {
-                strcpy(pUserData[i].parametro, "param2");
-            }
-            else if(strcmp(value, "param3") == 0)
-            {
-                strcpy(pUserData[i].parametro, "param3");
-            }
-            else if(strcmp(value, "param4") == 0)
-            {
-                strcpy(pUserData[i].parametro, "param4");
-            }
-            else
-            {
-                pUserData[0].errorState = WRONG_ARG; 
-                error = 1;
-            }
+            pUserData[i].parametro = "Hola soy param1";
         }
-        else if( strcmp(key, "-div") == 0)
+        else if(strcmp(value, "param2") == 0)
         {
-            pUserData[i].tipoDeArgumento = OPCION;
-            strcpy(pUserData[i].clave, "div");
-            pUserData[i].valor = atof(value);
+            pUserData[i].parametro = "Hola soy param2";
         }
-        else if( strcmp(key, "-sum") == 0)
+        else if(strcmp(value, "param3") == 0)
         {
-            pUserData[i].tipoDeArgumento = OPCION;
-            strcpy(pUserData[i].clave, "sum");
-            pUserData[i].valor = atof(value);
+            pUserData[i].parametro = "Hola soy param3";
         }
-        else if( strcmp(key, "-res") == 0)
+        else if(strcmp(value, "param4") == 0)
         {
-            pUserData[i].tipoDeArgumento = OPCION;
-            strcpy(pUserData[i].clave, "res");
-            pUserData[i].valor = atof(value);       // VER SI METIO MAL EL NUMERO por el atof !!!!!!
+            pUserData[i].parametro = "Hola soy param4";
         }
         else
         {
-            pUserData[0].errorState = WRONG_ARG;
+            pUserData[0].errorState = WRONG_ARG; 
             error = 1;
         }
     }
+    else if( strcmp(key, "-clave1") == 0)
+    {
+        pUserData[i].tipoDeArgumento = OPCION;
+        pUserData[i].clave = "Hola soy la clave 1";
+        pUserData[i].valor = atof(value);
+    }
+    else if( strcmp(key, "-clave2") == 0)
+    {
+        pUserData[i].tipoDeArgumento = OPCION;
+        pUserData[i].clave = "Hola soy la clave 2";
+        pUserData[i].valor = atof(value);
+    }
+    else if( strcmp(key, "-clave3") == 0)
+    {
+        pUserData[i].tipoDeArgumento = OPCION;
+        pUserData[i].clave = "Hola soy la clave 3";
+        pUserData[i].valor = atof(value);     
+    }
+    else
+    {
+        pUserData[0].errorState = WRONG_ARG;
+        error = 1;
+    }
+    
     return !error;   
 }
